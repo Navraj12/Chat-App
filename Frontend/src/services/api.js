6535da086030e97090048276527e59276693f9aa
Navraj Awasthi <awasthintr07@gmail.com>
2025-10-27 18:52:19 +0545

    solved some error in frontend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://chat-app-2-8xkv.onrender.com';
 
 const api = axios.create({
     baseURL: API_URL,
